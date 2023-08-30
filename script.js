55bcaaa76a8c5519d5ec3e539593658f59a60047
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-08-30 12:03:07 -0400

    adds header

---
+++ script.js
@@ -0,0 +1,4 @@
+var currentTimeDisplau = document.querySelector("#currentTime");
+var timeNow = dayjs().format(" MMMM DD, YYYY hh: mm: ss");
+currentTime.textContent = timeNow;
+console.log(timeNow);
